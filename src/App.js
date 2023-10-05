8e5986305530db55e074af7d2ab88e67af61715c
Mark Worth <markworth89@gmail.com>
2023-10-05 09:21:18 -0400

    Basic responsive ui

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="navbar">
+        
+      </div>
+      <div className="name-bar">LemonChat</div>
+      <div className="blur-pane"></div>
+      <div className="content-pane">
+        <div className="content-column">
+        <div className="content-row">
+            <div className="content-message">test1</div>
+          </div>
+          <div className="content-row">
+            <div className="content-message">test1</div>
+          </div>
+          <div className="content-row">
+            <div className="content-message">test1</div>
+          </div>
+          <div className="content-row">
+            <div className="content-message">test1</div>
+          </div>
+          <div className="content-row">
+            <div className="content-message">test1</div>
+          </div>
+          <div className="content-row">
+            <div className="content-message">test1</div>
+          </div>
+          <div className="content-row">
+            <div className="content-message">test1</div>
+          </div>
+          <div className="content-row">
+            <div className="content-message">test1</div>
+          </div>
+          <div className="content-row">
+            <div className="content-message">test1</div>
+          </div>
+          <div className="content-row">
+            <div className="content-message">test1</div>
+          </div>
+          <div className="content-row">
+            <div className="content-message">test1</div>
+          </div>
+          <div className="content-row">
+            <div className="content-message">test5</div>
+          </div>
+        </div>
+        <div className="content-column">
+          <div className="content-row">
+            <div className="content-message">test2</div>
+          </div>
+        </div>
+        <div className="content-column">
+          <div className="content-row">
+            <div className="content-message">test3</div>
+          </div>
+        </div>
+        <div className="content-column">
+          <div className="content-row">
+            <div className="content-message">test3</div>
+          </div>
+        </div>
+        <div className="content-column">
+          <div className="content-row">
+            <div className="content-message">test3</div>
+          </div>
+        </div>
+        <div className="content-column">
+          <div className="content-row">
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
